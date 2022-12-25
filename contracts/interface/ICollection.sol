30cbe443f68135f98a871f7bae26f0661df37d73
aquangvm <aquangvm@gmail.com>
2022-12-25 22:22:39 +0700

    update vesting

--- contracts/interface/ICollection.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.8.4;
-
-interface ICollection {
-    function safeMint(address owner, string memory tokenCID)
-        external
-        returns (uint256);
-}
--- contracts/interface/IFactory.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.8.4;
-
-interface IFactory {
-    function createCollection(
-        string memory _name,
-        string memory _sysbol,
-        string memory _collectionCID
-    ) external returns (address newCollection);
-}
